7b13f3e9ee45d18b55e6fe47b3708a11aec50d26
ndq04 <duyquang412000@gmail.com>
2021-11-18 10:26:16 +0700

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,20 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.home}>
-       <h2>Welcome to Gaming Vibes</h2>
+      <h2>Welcome to Gaming Vibes</h2>
       <div>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis deleniti rem aspernatur odit hic autem neque repellat alias? Debitis veniam inventore ipsum similique quos animi ipsa asperiores fuga dolor id. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis deleniti rem aspernatur odit hic autem neque repellat alias? Debitis veniam inventore ipsum similique quos animi ipsa asperiores fuga dolor id. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis deleniti rem aspernatur odit hic autem neque repellat alias? Debitis veniam inventore ipsum similique quos animi ipsa asperiores fuga dolor id.</p>
+        <p>
+          We don’t make assumptions about the rest of your
+          technology stack, so you can develop new features
+          in React without rewriting existing code. React
+          can also render on the server using Node and power
+          mobile apps using React Native. We don’t make
+          assumptions about the rest of your technology
+          stack, so you can develop new features in React
+          without rewriting existing code. React can also
+          render on the server using Node and power mobile
+          apps using React Native.
+        </p>
       </div>
     </div>
   )
